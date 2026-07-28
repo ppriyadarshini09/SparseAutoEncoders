7108a1033080e90e911bc99c0f0818f2897fd602
palakpaneer-in-nyc <payal.pri0994@gmail.com>
2026-07-28 06:47:38 -0400

    print statements

--- train_sae.py
+++ train_sae.py
@@ -44,16 +44,19 @@ def train(activations_path, config=sae_train_config, save_path=None):
     # Supply data to SAE.
     best_loss = float('inf')
     for epoch in range(config['epochs']):
+        if epoch % 10 == 0:
+            print(f"Epoch: {epoch}")
         for i, x in enumerate(get_batch(acts, config['batch_size'])):
             loss, metrics = sae.loss(x, config['l1_coeff'])
-            optimizer.zero_grad() # Reset all gradients.
-            loss.backward() # caculate new gradients.
-            optimizer.step() # update all tensor weights with new gradients.
-            sae.normalize_decoder_weights()
+            optimizer.zero_grad()   # Reset all gradients.
+            loss.backward()         # caculate new gradients.
+            optimizer.step()        # update all tensor weights with new gradients.
+            sae.normalize_decoder_weights() # address shrink f & grow w_d loophole
 
             if i % config['eval_every'] == 0:
-                if loss < best_loss:
-                    best_loss = loss
+                if loss < best_loss.item():
+                    best_loss = loss.item()
+                    print(f"Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}")
                     save_data = {
                         'sae_state_dict': sae.state_dict(),
                         'best_loss': best_loss,
